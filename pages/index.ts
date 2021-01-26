9aaaa6e800cc3122ba354b711b87a747b1d6f62e
South <1275093225@qq.com>
2021-01-26 16:03:12 +0800

    导出swagger页面数据

--- pages/index.ts
+++ pages/index.ts
@@ -3,6 +3,11 @@ import path from 'path';
 import puppeteer from 'puppeteer';
 import { url } from '../pages/utils/utils';
 
+interface IAPIData {
+  path: string | null | undefined;
+  method: string | undefined;
+  description: string | undefined;
+}
 class Analyzer{
   private static instance: Analyzer;
 
@@ -15,7 +20,11 @@ class Analyzer{
 
   private async initPage() {
     const browser = await puppeteer.launch({
-      headless: false
+      headless: false,
+      // defaultViewport: {
+      //   width: 2000,
+      //   height: 1000
+      // }
     });
     const page = await browser.newPage();
     await page.goto(url);
@@ -24,8 +33,8 @@ class Analyzer{
 
   private async getTitle(page: puppeteer.Page) {
     const label = '.wrapper > section > div > span > .opblock-tag-section > .opblock-tag > a > span';
-    await page.waitForSelector(label, { timeout: 0 });
-    const titles = await page.$$eval(label, (labels: Element[]) => labels.map((item: Element) => item.innerHTML));
+    await page.waitForSelector(label, { timeout: 5000 });
+    const titles = await page.$$eval(label, labels => labels.map(item => item.innerHTML));
     return titles;
   }
 
@@ -33,12 +42,30 @@ class Analyzer{
   private async openInfo(page: puppeteer.Page, titles: string[]) {
     for (let i = titles.length - 1; i >= 0; i--) {
       const label = `.wrapper > section > div > span > .opblock-tag-section > #operations-tag-${titles[i]}`;
-      await page.waitForSelector(label, { timeout: 3000 });
+      await page.waitForSelector(label, { timeout: 10000 });
       await page.click(label)
     }
   }
 
-  private async writeFiles(data: string[]) {
+  // 获取url路径
+  private async getAPIData(page: puppeteer.Page) {
+    const label = `.wrapper > section > div > span > .opblock-tag-section > .no-margin > span > .opblock > .opblock-summary`;
+    await page.waitForSelector(label, { timeout: 10000 });
+    const APIData = await page.$$eval(label, labels => {
+      const data: IAPIData[] = [];
+      labels.map(item => {
+        const method = item.querySelector('.opblock-summary-method')?.innerHTML;
+        const path = item.querySelector('.opblock-summary-path')?.getAttribute('data-path');
+        const description = item.querySelector('.opblock-summary-description')?.innerHTML;
+        data.push({ method, path, description })
+      })
+      return data;
+    });
+
+    return APIData;
+  }
+
+  private async writeFiles(data: IAPIData[]) {
     return await fs.writeFileSync("./data/data.json", JSON.stringify(data, null, "\t"));
   }
 
@@ -46,9 +73,10 @@ class Analyzer{
     const page = await this.initPage();
     const titles = await this.getTitle(page);
     await this.openInfo(page, titles);
+    const data = await this.getAPIData(page)
     // await page.close();
 
-    await this.writeFiles(titles)
+    await this.writeFiles(data)
   }
 
   constructor() {
